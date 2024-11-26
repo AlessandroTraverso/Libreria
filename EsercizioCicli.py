ff34b46ab1e3c35f7d87d9f3fbdc5efca4328adb
AlessandroTraverso <alino.traverso@gmail.com>
2024-11-26 12:18:47 +0100

    print

---
+++ EsercizioCicli.py
@@ -0,0 +1,8 @@
+#Esercizio cicli
+
+x = 6
+while(x>0):
+    for i in range(6):
+        x-=1
+        print("Contatore:",i)
+        print("Numero inserito:",x)
---
+++ EsercizioInput.py
@@ -0,0 +1,5 @@
+#Esercizio input
+
+x = int(input("Inserisci un numero"))
+y = input("Come va?")
+z = float(input("Inserisci un numero con la virgola"))
