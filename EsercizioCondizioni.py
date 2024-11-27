3e3b382df899df908efd42603384293cc77633b4
Alessandro <alino.traverso@gmail.com>
2024-11-27 10:49:22 +0100

    Delete EsercizioCondizioni.py

--- EsercizioCondizioni.py
+++
@@ -1,9 +0,0 @@
-#Esercizio condizioni
-
-x = int(input("Inserisci un numero"))
-if(x>10):
-    print("Il numero inserito",x,"è maggiore di 10")
-elif(x<5):
-    print("Il numero inserito",x,"è minore di 5")
-else:
-    print("Il numero inserito",x,"è compreso tra 5 e 10")
